75ba15ea01c2179699b82d1580325f081d9aacc8
Johny Xu <ahstuxq@gmail.com>
2022-06-19 11:44:12 +1000

    add share component

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './share.css';
+import PermMediaIcon from '@mui/icons-material/PermMedia';
+import TagIcon from '@mui/icons-material/Tag';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+          <input placeholder="What's your mind Johny?" className="shareInput" />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMediaIcon htmlColor="tomato" className="shareIcon" />
+              <div className="shareOptionText">Photo or Video</div>
+            </div>
+            <div className="shareOption">
+              <TagIcon htmlColor="blue" className="shareIcon" />
+              <div className="shareOptionText">Tag</div>
+            </div>
+            <div className="shareOption">
+              <AddLocationIcon htmlColor="green" className="shareIcon" />
+              <div className="shareOptionText">Location</div>
+            </div>
+            <div className="shareOption">
+              <InsertEmoticonIcon htmlColor="goldenrod" className="shareIcon" />
+              <div className="shareOptionText">Feelings</div>
+            </div>
+          </div>
+          <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+}
