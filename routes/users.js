0b14c3a20c2b0d156d475279ef43f4d0f48b7500
Johny Xu <ahstuxq@gmail.com>
2022-06-21 23:51:43 +1000

    update profile user post & get user by query parameter

--- routes/users.js
+++ routes/users.js
@@ -45,9 +45,12 @@ router.delete('/:id', async (req, res) => {
 });
 
 //get a user
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const userId = req.query.userId;
+    const username = req.query.username;
+
+    const user = userId ? await User.findById(userId) : await User.findOne({ username: username });
     const { password, updatedAt, ...rest } = user._doc;
     res.status(200).json(rest);
   } catch (error) {
