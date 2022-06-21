3a677ce34e58d2c704145220c501538f0f1c08c5
Johny Xu <ahstuxq@gmail.com>
2022-06-21 23:51:43 +1000

    update profile user post & get user by query parameter
    (cherry picked from commit 0b14c3a20c2b0d156d475279ef43f4d0f48b7500)

--- routes/posts.js
+++ routes/posts.js
@@ -67,9 +67,9 @@ router.get('/:id', async (req, res) => {
 });
 
 //get timeline posts
-router.get('/timeline/all', async (req, res) => {
+router.get('/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
@@ -82,4 +82,17 @@ router.get('/timeline/all', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+//get user's all posts
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
