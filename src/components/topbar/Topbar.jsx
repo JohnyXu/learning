1b6f6792b6014abe0bbb49647a1d2f9bf3f2a45a
Johny Xu <ahstuxq@gmail.com>
2022-07-16 18:13:34 +1000

    add topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './topbar.css';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import LanguageIcon from '@material-ui/icons/Language';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 export default function Topbar() {
   return (
@@ -10,12 +12,22 @@ export default function Topbar() {
           <span className="logo">FlyingBird</span>
         </div>
         <div className="topRight">
-          <div className="topbarIcons">
-            <div className="topbarIconContainer">
-              <NotificationsNoneIcon />
-              <span className="topIconBage">2</span>
-            </div>
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
           </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/12823101/pexels-photo-12823101.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
