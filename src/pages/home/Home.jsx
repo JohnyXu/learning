88610491f51baf9e38af7b498d6490f089f8a767
Johny Xu <ahstuxq@gmail.com>
2022-07-16 20:33:31 +1000

    add user analytics chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Chart from '../../components/chart/Chart';
 import FeatureInfo from '../../components/featureInfo/FeatureInfo';
+import { userData } from '../../dummyData';
 import './home.css';
 
 export default function Home() {
   return (
     <div className="home">
       <FeatureInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
     </div>
   );
 }
