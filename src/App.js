ac9bda17116ebd50277e31743f482b1c4d538716
Johny Xu <ahstuxq@gmail.com>
2022-07-16 23:14:18 +1000

    add userList with material data grid component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="users/" element={<UserList />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
