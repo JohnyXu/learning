b0a97e7bcd126bcfb35022b1170457c7ef4f8eb2
Johny Xu <ahstuxq@gmail.com>
2022-07-16 18:53:40 +1000

    add sidebar component with material icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others"> other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import LineStyleIcon from '@material-ui/icons/LineStyle';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import WorkOutlineIcon from '@material-ui/icons/WorkOutline';
+import ReportIcon from '@material-ui/icons/Report';
+
+import './sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PermIdentityIcon className="sidebarIcon" />
+              Users
+            </li>
+
+            <li className="sidebarListItem">
+              <StorefrontIcon className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChartIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutlineIcon className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedIcon className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutlineIcon className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutlineIcon className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
