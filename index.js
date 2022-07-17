c6490fa0f6fb374aca8793df94c3110a866264c7
Johny Xu <ahstuxq@gmail.com>
2022-07-17 12:06:39 +1000

    remove unused file to setup new projects

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const userRoute = require('./routes/users');
-const authRoute = require('./routes/auth');
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {}, () => {
-  console.log('connected to MongoDB');
-});
-
 app.use(express.json());
-app.use(helmet());
-app.use(morgan('common'));
 
 app.get('/', (req, res) => {
   res.send('welcome to homepage');
@@ -23,9 +12,6 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
   res.send('welcome to user page');
 });
-
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('backend is running!');
