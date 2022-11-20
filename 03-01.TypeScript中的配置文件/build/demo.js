c68bd8a083fca3553f0afbdb80268e28f9b1f63f
Johny Xu <ahstuxq@gmail.com>
2022-11-20 22:03:41 +1100

    更新配置文件

---
+++ 03-01.TypeScript中的配置文件/build/demo.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.name = void 0;
+var teacher = null;
+function fn(name, age) {
+    console.log(age);
+}
+exports.name = 'dell2';
