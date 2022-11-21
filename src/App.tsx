b30b04c79bf9cc8288708b3f0124b8a297bf3a62
Johny Xu <ahstuxq@gmail.com>
2022-11-21 12:08:06 +1100

    add button component & storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Button>Default Button</Button>
+      <Button btnType="primary">Default Button</Button>
+    </div>
+  );
 }
 
 export default App;
