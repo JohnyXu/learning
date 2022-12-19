e7c8c839031e76c13808286e517a06c759ee2f8c
Johny Xu <ahstuxq@gmail.com>
2022-12-19 13:04:49 +1100

    add version api with express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // In src/index.js
 const express = require('express');
+const v1Router = require('./v1/routes');
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// For testing purposes
-app.get('/', (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
+app.use('/api/v1', v1Router);
+app.use('/api/v1/workouts', v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
