e622268336cf95221ddcc01adb0833c2cf421ecf
Johny Xu <ahstuxq@gmail.com>
2022-12-19 17:48:25 +1100

    how to use ErrorBoundary in react component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { ErrorBoundary } from 'react-error-boundary';
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  console.log(error);
+  console.log(resetErrorBoundary);
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+function Counter(props) {
+  throw new Error('Simulated error.');
+}
+
 function App() {
-  return <div className="App">hello</div>;
+  const ui = (
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        console.log('onReset');
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <Counter />
+    </ErrorBoundary>
+  );
+
+  const enable = true;
+  return <div className="App">hello{enable ? ui : <p>test</p>}</div>;
 }
 
 export default App;
